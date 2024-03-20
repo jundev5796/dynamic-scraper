204027dd82523bb6e8e3df42388f97ae8b920dfd
HyunJun Baek <hjbaek5796@gmail.com>
2024-03-21 01:50:03 +0900

    Implement csv to create spreadsheet

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from playwright.sync_api import sync_playwright
 import time
 from bs4 import BeautifulSoup
+import csv
 
 p = sync_playwright().start()
 
@@ -55,5 +56,16 @@ for job in jobs:
   }
   jobs_db.append(job)
 
-print(jobs_db)
-print(len(jobs_db))
+
+file = open("jobs.csv", "w")
+writer = csv.writer(file)
+writer.writerow([
+  "Title", 
+  "Company", 
+  # "Location", 
+  "Reward", 
+  "Link",
+])
+for job in jobs_db:
+  writer.writerow(job.values())
+file.close()
